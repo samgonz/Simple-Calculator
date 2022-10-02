b5bd30700b3bfbdd7e4dc1473b199b27ddb6816c
samgonz <doomteam1@gmail.com>
2022-10-01 23:46:28 -0500

    Changed to allow unlimited arguments

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from tokenize import String
 import calculate
 import splashLogo
+
+continue_math = ''
 
 #Initial Splash Screen
 print(splashLogo.logo)
@@ -7,9 +10,22 @@ print(splashLogo.logo)
 #User inputs
 first_number = float(input('What is the first number?\n'))
 operator = input('What operator would you like to use?\n')
-last_number = float(input('What is the last number?\n'))
+next_number = float(input('What is the next number?\n'))
+result = calculate.calculate(first_number, operator, next_number)
+print(f'{first_number} {operator} {next_number} = {result}\n')
 
-#Result of the math operation
-result = calculate.calculate(first_number, operator, last_number)
+while continue_math.lower() != 'n':
+    #Result of the math operation
 
-print(f'{first_number} {operator} {last_number} = {result}')
+    continue_math = input('Would you like to enter another number? [Y or N]\n')
+    
+    if continue_math.lower() != 'n':
+        operator = input('What operator would you like to use?\n')
+        next_number = float(input('What is the next number?\n'))
+    elif continue_math.lower() == 'n':
+        print(f'{f_number} {operator} {next_number} = {result}\n')
+        break
+    
+    f_number = result
+    result = calculate.calculate(f_number, operator, next_number)
+    print(f'{f_number} {operator} {next_number} = {result}\n')  
